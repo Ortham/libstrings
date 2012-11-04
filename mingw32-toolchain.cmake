0209a360da299ae43e01f18b41ab04c4324f4769
WrinklyNinja <wrinklyninja1@gmail.com>
2012-11-04 23:08:58 +0000

    Finally got building of 32 and 64 bit Windows (cross-compiled) shared libraries working.

--- mingw32-toolchain.cmake
+++ mingw32-toolchain.cmake
@@ -2,14 +2,16 @@
 set (CMAKE_SYSTEM_NAME Windows)
 
 IF (LIBSTR_ARCH MATCHES "32")
-    set (CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-    set (CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-    set (CMAKE_RC_COMPILER i586-mingw32msvc-windres)
+    set (MINGW i586-mingw32msvc)
 ELSE (LIBSTR_ARCH MATCHES "32")
-    set (CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-    set (CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-    set (CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+    set (MINGW x86_64-w64-mingw32)
 ENDIF (LIBSTR_ARCH MATCHES "32")
+
+set (CMAKE_C_COMPILER   ${MINGW}-gcc)
+set (CMAKE_CXX_COMPILER ${MINGW}-g++)
+set (CMAKE_RC_COMPILER  ${MINGW}-windres)
+set (CMAKE_RANLIB       ${MINGW}-ranlib)
+#set (CMAKE_FORCE_AR           ${MINGW}-ar)
 
 IF (${LIBSTR_LINK} MATCHES "STATIC")
     add_definitions (-DLIBSTRINGS_STATIC)
